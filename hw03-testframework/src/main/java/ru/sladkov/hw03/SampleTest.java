90373955ffdc2517b4eb6781483fe6a8cde39fe6
Mikhail Sladkov <nbsmm@softwerke.local>
2022-01-27 12:53:58 +0300

    hw03: enchanced code

---
+++ hw03-testframework/src/main/java/ru/sladkov/hw03/SampleTest.java
@@ -0,0 +1,45 @@
+package ru.sladkov.hw03;
+
+import ru.sladkov.hw03.annotations.After;
+import ru.sladkov.hw03.annotations.Before;
+import ru.sladkov.hw03.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SampleTest {
+
+    @Before
+    public void setUp() {
+        System.out.println("--- @Before called in test class " + this.toString() + " ---");
+//        throw new RuntimeException();
+    }
+
+    @Test
+    public void passingTest1() {
+        System.out.println("--- passingTest1 called in test class " + this.toString() + " ---");
+    }
+
+    @Test
+    public void passingTest2() {
+        System.out.println("--- passingTest2 called in test class " + this.toString() + " ---");
+        assertThat(1 + 1).isEqualTo(2);
+    }
+
+    @Test
+    public void failingTest1() {
+        System.out.println("--- failingTest1 called in test class " + this.toString() + " ---");
+        throw new NullPointerException();
+    }
+
+    @Test
+    public void failingTest2() {
+        System.out.println("--- failingTest2 called in test class " + this.toString() + " ---");
+        assertThat(1 + 1).isEqualTo(3);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("--- @After called in test class " + this.toString() + " ---");
+        throw new RuntimeException();
+    }
+}
--- hw03-testframework/src/main/java/ru/sladkov/hw03/TestsExecutor.java
+++ hw03-testframework/src/main/java/ru/sladkov/hw03/TestsExecutor.java
@@ -17,6 +17,7 @@ public class TestsExecutor {
         int passedTests = 0;
         int failedTests = 0;
 
+        //Get @Before, @Test and @After methods
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
             Annotation[] annotations = method.getAnnotations();
@@ -29,8 +30,10 @@ public class TestsExecutor {
             }
         }
 
+        //Execute tests
         System.out.println(clazz.getName() + ":");
         for (Method testMethod : testMethods) {
+            System.out.println("Executing " + testMethod.getName() + ":");
             boolean isTestPassed = executeTest(clazz, testMethod, beforeMethods, afterMethods);
             if (isTestPassed) {
                 passedTests++;
@@ -48,67 +51,72 @@ public class TestsExecutor {
 
     private static boolean executeTest(Class<?> clazz, Method testMethod, List<Method> beforeMethods,
                                        List<Method> afterMethods) {
+        //Create new class instance
         Object classInstance;
         try {
             classInstance = clazz.getConstructor().newInstance();
         } catch (Exception e) {
-            System.out.println("Can't instantiate new test class object" + e.toString());
+            System.out.println("Can't instantiate new test class object" + e.getCause().toString());
             return false;
         }
 
+        //Execute @Before methods
         try {
             executeMethods(classInstance, beforeMethods);
         } catch (Exception e) {
-            System.out.println("Can't execute @Before methods:" + e.toString());
-            executeSafeAfters(classInstance, afterMethods);
+            System.out.println("Can't execute @Before methods:" + e.getCause().toString());
+            executeAftersWithoutThrowingException(classInstance, afterMethods);
             return false;
         }
 
+        //Execute current @Test method
         try {
             executeMethod(classInstance, testMethod);
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException e) {
-            System.out.println("Error in initializing test: " + e.toString());
-            executeSafeAfters(classInstance, afterMethods);
-            return false;
         } catch (InvocationTargetException e) {
             Throwable targetException = e.getTargetException();
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             targetException.printStackTrace(pw);
-            System.out.println("Exception: " + sw.toString());
-            executeSafeAfters(classInstance, afterMethods);
+            System.out.println("Exception: " + sw);
+            executeAftersWithoutThrowingException(classInstance, afterMethods);
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error in initializing test: " + e.getCause().toString());
+            executeAftersWithoutThrowingException(classInstance, afterMethods);
             return false;
         }
 
+        //Execute @After methods
         try {
             executeMethods(classInstance, afterMethods);
         } catch (Exception e) {
-            System.out.println("Can't execute @After methods:" + e.toString());
+            System.out.println("Can't execute @After methods:" + e.getCause().toString());
             return false;
         }
 
         return true;
     }
 
-    private static void executeMethods(Object classInstance, List<Method> methods) throws NoSuchMethodException,
-            InvocationTargetException, InstantiationException, IllegalAccessException {
+    private static void executeMethods(Object classInstance, List<Method> methods) throws
+            InvocationTargetException, IllegalAccessException {
         for (Method method : methods) {
             method.invoke(classInstance);
         }
     }
 
-    private static void executeMethod(Object classInstance, Method method) throws NoSuchMethodException,
-            InvocationTargetException, InstantiationException, IllegalAccessException {
+    private static void executeMethod(Object classInstance, Method method) throws InvocationTargetException,
+            IllegalAccessException {
         method.invoke(classInstance);
     }
 
-    private static void executeSafeAfters(Object classInstance, List<Method> afterMethods) {
+    //If @Before or @Test threw exception - execute @After anyway
+    private static void executeAftersWithoutThrowingException(Object classInstance, List<Method> afterMethods) {
         try {
             for (Method afterMethod : afterMethods) {
                 afterMethod.invoke(classInstance);
             }
         } catch (Exception e) {
-            System.out.println("Exception in callAfters()");
+            System.out.println("Exception in executeAftersWithoutThrowingException() " + e.getCause().toString());
         }
     }
 }
