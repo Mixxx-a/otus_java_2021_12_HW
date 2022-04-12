386b52fb992f7cad37d05414f9bb9602ee381143
Mixxx-a <Mixxx-a@users.noreply.github.com>
2022-04-13 02:41:41 +0300

    hw-05: fix issues

--- hw05-logging/src/main/java/ru/sladkov/hw05/TestLoggingLoader.java
+++ hw05-logging/src/main/java/ru/sladkov/hw05/TestLoggingLoader.java
@@ -6,14 +6,16 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class TestLoggingLoader {
 
     private TestLoggingLoader() {}
 
-    static TestLoggingInterface createClass() {
-        InvocationHandler invocationHandler = new MyInvocationHandler(new TestLoggingImpl());
+    static TestLoggingInterface createClass(TestLoggingInterface testLoggingImpl) {
+        InvocationHandler invocationHandler = new MyInvocationHandler(testLoggingImpl);
         return (TestLoggingInterface) Proxy.newProxyInstance(TestLoggingLoader.class.getClassLoader(),
                 new Class<?>[]{TestLoggingInterface.class}, invocationHandler);
     }
@@ -21,17 +23,25 @@ class TestLoggingLoader {
     static class MyInvocationHandler implements InvocationHandler {
 
         private final TestLoggingInterface testLogging;
+        private final List<Method> loggedMethods = new ArrayList<>();
 
         MyInvocationHandler(TestLoggingInterface testLogging) {
             this.testLogging = testLogging;
+            Method[] methods = testLogging.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                Annotation logAnnotation = method.getDeclaredAnnotation(Log.class);
+                if (logAnnotation != null) {
+                    this.loggedMethods.add(method);
+                }
+            }
+
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             Method implementedMethod = testLogging.getClass().getDeclaredMethod(method.getName(),
                     method.getParameterTypes());
-            Annotation[] annotations = implementedMethod.getDeclaredAnnotations();
-            if (Arrays.stream(annotations).anyMatch(annotation -> annotation.annotationType() == Log.class)) {
+            if (this.loggedMethods.contains(implementedMethod)) {
                 System.out.println("executed method: " + method.getName() + ", param: " + Arrays.toString(args));
             }
             return method.invoke(testLogging, args);
